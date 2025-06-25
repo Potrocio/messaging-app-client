f7df31297eda52dd634958a146e2f09d944b9526
Potrocio <57481913+Potrocio@users.noreply.github.com>
2025-06-25 02:10:59 -0400

    add react router routes, create route pages and add them to the correct routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,61 @@
+// React & Dom
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+
+// Global CSS
 import './index.css'
-import App from './App.jsx'
+
+// React Router
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+
+// Route pages
+import LandingPage from './pages/LandingPage'
+import LoginPage from './pages/LoginPage'
+import SignupPage from './pages/SignupPage'
+import HomePage from './pages/HomePage'
+import SettingsPage from './pages/SettingsPage'
+import AddFriendPage from './pages/AddFriendPage'
+import NewMessagePage from './pages/NewMessagePage'
+import MessagePreviewPage from './pages/MessagePreviewPage'
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <LandingPage />
+  },
+  {
+    path: "/login",
+    element: <LoginPage />
+  },
+  {
+    path: "/signup",
+    element: <SignupPage />
+  },
+  {
+    path: "/home",
+    element: <HomePage />
+  },
+  {
+    path: "/settings",
+    element: <SettingsPage />
+  },
+  {
+    path: "/add-friend",
+    element: <AddFriendPage />
+  },
+  {
+    path: "/new-message",
+    element: <NewMessagePage />
+  },
+  {
+    path: "/message-preview",
+    element: <MessagePreviewPage />
+  },
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
