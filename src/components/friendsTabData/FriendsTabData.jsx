1fa894962fdaf2a5d5f1af876d9f2f0801155d76
Potrocio <57481913+Potrocio@users.noreply.github.com>
2025-06-29 00:53:38 -0400

    Add friends pending component skeleton and basic functionality

--- src/components/friendsTabData/FriendsTabData.jsx
+++ src/components/friendsTabData/FriendsTabData.jsx
@@ -8,6 +8,23 @@ export default function FriendsTabData() {
     const [menuOpened, setMenuOpened] = useState(false)
     const [removeFriendOn, setRemoveFriendOn] = useState(false)
     const [friendToRemove, setFriendToRemove] = useState({})
+    const [showPending, setShowPending] = useState(false)
+    const [fakePendingList, setFakePendingList] = useState({
+        pendingSent: [
+            {
+                id: 1,
+                firstName: "first",
+                lastName: "person",
+            }
+        ],
+        pendingReceived: [
+            {
+                id: 2,
+                firstName: "second",
+                lastName: "person",
+            }
+        ]
+    })
 
 
     const [friends, setFriends] = useState([
@@ -51,17 +68,28 @@ export default function FriendsTabData() {
         navigate('/add-friend')
     }
 
+    function handlePendingClick() {
+        setShowPending(true)
+        setMenuOpened(false)
+    }
+
+    function handleBackClick() {
+        setShowPending(false)
+    }
+
     return (
         <div className={styles.contentWrapper}>
-            {menuOpened && <div className={styles.backdrop}></div>}
             <button className={styles.optionsButton} onClick={handleShowOptions}>+</button>
+
+            {menuOpened && <div className={styles.backdrop}></div>}
             {menuOpened &&
                 <div className={styles.optionsWrapper}>
                     <button className={styles.closeMenuButton} onClick={handleMenuClose}>X</button>
                     <button onClick={handleAddFriendClick}>Add friend</button>
-                    <button>Pending friends</button>
+                    <button onClick={handlePendingClick}>Pending friends</button>
                     <button onClick={handleToggleRemoveFriends}>Remove a friend</button>
                 </div>}
+
             {friendToRemove.name && <div className={styles.backdrop}></div>}
             {friendToRemove.id &&
                 <div className={styles.optionsWrapper}>
@@ -72,18 +100,49 @@ export default function FriendsTabData() {
                     </div>
                 </div>
             }
-            <ul className={styles.friendsWrapper}>
-                {friends.map(friend => {
-                    return (
-                        <>
-                            <li key={friend.id} onClick={() => handleFriendClick(friend.id)}>
-                                <p>{friend.name}</p>
-                            </li>
-                            {removeFriendOn && <button onClick={() => handleRemoveFriend(friend)}>Remove</button>}
-                        </>
-                    )
-                })}
-            </ul>
+
+            {showPending ?
+                (
+                    <div className={styles.pendingWrapper}>
+                        <button onClick={handleBackClick}>Back</button>
+                        <ul>
+                            {fakePendingList.pendingReceived.map(request => {
+                                return (
+                                    <>
+                                        <li>{request.firstName + ' ' + request.lastName}</li>
+                                        <button>Accept</button>
+                                        <button>Reject</button>
+                                    </>
+                                )
+                            })}
+                            {fakePendingList.pendingSent.map(request => {
+                                return (
+                                    <>
+                                        <li>{request.firstName + ' ' + request.lastName}</li>
+                                        <div>Pending</div>
+                                    </>
+                                )
+                            })}
+                        </ul>
+                    </div>
+                )
+                :
+                (
+                    <ul className={styles.friendsWrapper}>
+                        {friends.map(friend => {
+                            return (
+                                <>
+                                    <li key={friend.id} onClick={() => handleFriendClick(friend.id)}>
+                                        <p>{friend.name}</p>
+                                    </li>
+                                    {removeFriendOn && <button onClick={() => handleRemoveFriend(friend)}>Remove</button>}
+                                </>
+                            )
+                        })}
+                    </ul>
+                )
+            }
+
         </div>
     )
 }
