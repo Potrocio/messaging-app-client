465e406405907ebbc926a7cdccc6f80d36e21d2c
Potrocio <57481913+Potrocio@users.noreply.github.com>
2025-07-13 22:40:11 -0400

    Add createContext to homepage page, Create fetch get request for settings

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState } from "react"
+import { createContext, useEffect, useState } from "react"
 import styles from "./pages.module.css"
 import HomeTabSelector from "../components/homeTabSelector/HomeTabSelector"
 import SelectedTabData from "../components/selectedTabData/SelectedTabData"
@@ -6,7 +6,11 @@ import { useNavigate } from "react-router-dom"
 
 export default function HomePage() {
     const [tabSelected, setTabSelected] = useState('Messages')
+    const [conversationsPreview, setConversationsPreview] = useState([])
+    const [friends, setFriends] = useState([])
 
+
+    const myContext = createContext()
     const navigate = useNavigate()
 
     function navigateToSettings() {
@@ -14,17 +18,49 @@ export default function HomePage() {
     }
 
     function handleLogOut() {
+        // Eventually this will log out the person
         navigate('/')
+        localStorage.removeItem("token")
     }
 
+    useEffect(() => {
+        async function fetchDashboard() {
+            try {
+                const token = localStorage.getItem("token")
+                if (token) {
+                    const apiUrl = import.meta.env.VITE_API_URL;
+                    const res = await fetch(`${apiUrl}/api/user/dashboard`, {
+                        mode: "cors",
+                        method: "GET",
+                        headers: { "Authorization": `Bearer ${token}` }
+                    })
+                    if (!res.ok) throw new Error(res.status)
+                    const data = await res.json()
+
+                    setConversationsPreview(data.conversationsPreview)
+                    setFriends(data.friends)
+                } else {
+                    navigate('/login')
+                }
+            } catch (error) {
+                console.log("Error", error)
+            }
+        }
+        fetchDashboard()
+
+    }, [tabSelected])
+
+    // Add tabselected and settabselected ot the context as well
     return (
-        <div className={styles.homepageWrapper}>
-            <div>
-                <button className={styles.settings} onClick={navigateToSettings}>Settings</button>
-                <button onClick={handleLogOut}>Log out</button>
+        <myContext.Provider value={{ conversationsPreview, friends }}>
+            <div className={styles.homepageWrapper}>
+                <div>
+                    <button className={styles.settings} onClick={navigateToSettings}>Settings</button>
+                    <button onClick={handleLogOut}>Log out</button>
+                </div>
+                <HomeTabSelector tabSelected={tabSelected} setTabSelected={setTabSelected} />
+                <SelectedTabData tabSelected={tabSelected} />
             </div>
-            <HomeTabSelector tabSelected={tabSelected} setTabSelected={setTabSelected} />
-            <SelectedTabData tabSelected={tabSelected} />
-        </div>
+        </myContext.Provider>
     )
 }
--- src/pages/SettingsPage.jsx
+++ src/pages/SettingsPage.jsx
@@ -1,11 +1,11 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import styles from "./pages.module.css"
 import { useNavigate } from "react-router-dom"
 
 export default function SettingsPage() {
-    const [email, setEmail] = useState('john.something@email.com')
-    const [firstName, setFirstName] = useState('john')
-    const [lastName, setLastName] = useState('feliciano')
+    const [email, setEmail] = useState('')
+    const [firstName, setFirstName] = useState('')
+    const [lastName, setLastName] = useState('')
     const [password, setPassword] = useState('')
     const [confirmPassword, setConfirmPassword] = useState('')
     const [submitMessage, setSubmitMessage] = useState('')
@@ -14,8 +14,43 @@ export default function SettingsPage() {
     const [editFirstName, setEditFirstName] = useState(false)
     const [editLastName, setEditLastName] = useState(false)
     const [editPassword, setEditPassword] = useState(false)
+    const [loading, setLoading] = useState(true)
+
 
     const navigate = useNavigate()
+
+    useEffect(() => {
+        async function fetchSettings() {
+            try {
+                const token = localStorage.getItem("token")
+                if (token) {
+                    const apiUrl = import.meta.env.VITE_API_URL;
+                    const res = await fetch(`${apiUrl}/api/user/settings`, {
+                        mode: "cors",
+                        method: "GET",
+                        headers: {
+                            "Authorization": `Bearer ${token}`
+                        }
+                    })
+                    if (!res.ok) throw new Error(res.status)
+                    const data = await res.json()
+                    console.log(data.user)
+                    if (data.user) {
+                        setFirstName(data.user.firstName)
+                        setLastName(data.user.lastName)
+                        setEmail(data.user.email)
+                    }
+                } else {
+                    navigate('/login')
+                }
+            } catch (error) {
+                console.log(error)
+            } finally {
+                setLoading(false)
+            }
+        }
+        fetchSettings()
+    }, [])
 
     function handleEmailChange(e) {
         setEmail(e.target.value)
@@ -57,7 +92,7 @@ export default function SettingsPage() {
         setSubmitMessage('')
     }
 
-    function saveSettings() {
+    function handleSubmit() {
         if (password === confirmPassword) {
             setSubmitMessage("Changes saved")
             setEditEmail(false)
@@ -74,89 +109,95 @@ export default function SettingsPage() {
     }
 
     return (
-        <div className={styles.pageWrapper}>
-            <button className={styles.settingsHomeButton} onClick={cancelSettings}>Back to home</button>
-            <div>
-                {editEmail ?
-                    (<>
-                        <label htmlFor="email">Email</label>
-                        <input
-                            type="email"
-                            id="email"
-                            name="email"
-                            value={email}
-                            onChange={handleEmailChange}
-                        />
-                    </>)
-                    :
-                    (<div>{email}</div>)
-                }
-                {editEmail || <button className={styles.editButton} onClick={emailEditMode}>Edit</button>}
-            </div>
-            <div>
-                {editFirstName ?
-                    (<>
-                        <label htmlFor="firstName">First name</label>
-                        <input
-                            type="text"
-                            id="firstName"
-                            name="firstName"
-                            value={firstName}
-                            onChange={handleFirstNameChange}
-                        />
-                    </>)
-                    :
-                    (<div>{firstName}</div>)
-                }
-                {editFirstName || <button className={styles.editButton} onClick={firstNameEditMode}>Edit</button>}
-            </div>
-            <div>
-                {editLastName ?
-                    (<>
-                        <label htmlFor="lastName">Last name</label>
-                        <input
-                            type="text"
-                            id="lastName"
-                            name="lastName"
-                            value={lastName}
-                            onChange={handleLastNameChange}
-                        />
-                    </>)
-                    :
-                    (<div>{lastName}</div>)
-                }
-                {editLastName || <button className={styles.editButton} onClick={lastNameEditMode}>Edit</button>}
-            </div>
-            <div>
-                {editPassword &&
-                    <>
-                        <label htmlFor="password">Password</label>
-                        <input
-                            type="password"
-                            id="password"
-                            name="password"
-                            value={password}
-                            onChange={handlePasswordChange}
-                        />
+        <>
 
-                        <label htmlFor="confirmPassword">Confirm password</label>
-                        <input
-                            type="password"
-                            id="confirmPassword"
-                            name="confirmPassword"
-                            value={confirmPassword}
-                            onChange={handleConfirmPasswordChange}
-                        />
-                        {passwordsMatchMessage && <div>{passwordsMatchMessage}</div>}
-                    </>
-                }
-                {editPassword || <button className={styles.editButton} onClick={passwordEditMode}>Change password</button>}
-            </div>
-            {submitMessage && <div>{submitMessage}</div>}
-            <div className={styles.settingsActionWrapper}>
-                <button className={styles.saveButton} onClick={saveSettings}>Save changes</button>
-                <button className={styles.cancelButton} onClick={cancelSettings}>Cancel changes</button>
-            </div>
-        </div>
+            {loading && <div>Loading</div>}
+            {!loading &&
+                <div className={styles.pageWrapper}>
+                    <button className={styles.settingsHomeButton} onClick={cancelSettings}>Back to home</button>
+                    <div>
+                        {editEmail ?
+                            (<>
+                                <label htmlFor="email">Email</label>
+                                <input
+                                    type="email"
+                                    id="email"
+                                    name="email"
+                                    value={email}
+                                    onChange={handleEmailChange}
+                                />
+                            </>)
+                            :
+                            (<div>{email}</div>)
+                        }
+                        {editEmail || <button className={styles.editButton} onClick={emailEditMode}>Edit</button>}
+                    </div>
+                    <div>
+                        {editFirstName ?
+                            (<>
+                                <label htmlFor="firstName">First name</label>
+                                <input
+                                    type="text"
+                                    id="firstName"
+                                    name="firstName"
+                                    value={firstName}
+                                    onChange={handleFirstNameChange}
+                                />
+                            </>)
+                            :
+                            (<div>{firstName}</div>)
+                        }
+                        {editFirstName || <button className={styles.editButton} onClick={firstNameEditMode}>Edit</button>}
+                    </div>
+                    <div>
+                        {editLastName ?
+                            (<>
+                                <label htmlFor="lastName">Last name</label>
+                                <input
+                                    type="text"
+                                    id="lastName"
+                                    name="lastName"
+                                    value={lastName}
+                                    onChange={handleLastNameChange}
+                                />
+                            </>)
+                            :
+                            (<div>{lastName}</div>)
+                        }
+                        {editLastName || <button className={styles.editButton} onClick={lastNameEditMode}>Edit</button>}
+                    </div>
+                    <div>
+                        {editPassword &&
+                            <div className={styles.settingsPasswordWrapper}>
+                                <label htmlFor="password">Password</label>
+                                <input
+                                    type="password"
+                                    id="password"
+                                    name="password"
+                                    value={password}
+                                    onChange={handlePasswordChange}
+                                />
+
+                                <label htmlFor="confirmPassword">Confirm password</label>
+                                <input
+                                    type="password"
+                                    id="confirmPassword"
+                                    name="confirmPassword"
+                                    value={confirmPassword}
+                                    onChange={handleConfirmPasswordChange}
+                                />
+                                {passwordsMatchMessage && <div>{passwordsMatchMessage}</div>}
+                            </div>
+                        }
+                        {editPassword || <button className={styles.editButton} onClick={passwordEditMode}>Change password</button>}
+                    </div>
+                    {submitMessage && <div>{submitMessage}</div>}
+                    <div className={styles.settingsActionWrapper}>
+                        <button className={styles.saveButton} onClick={handleSubmit}>Save changes</button>
+                        <button className={styles.cancelButton} onClick={cancelSettings}>Cancel changes</button>
+                    </div>
+                </div>
+            }
+        </>
     )
 }
