15fceb50bd6f1c88261519ee12760d3e88ee0c32
Potrocio <57481913+Potrocio@users.noreply.github.com>
2025-07-17 04:14:43 -0400

    Remove test data from function and useContext with real conversation data

--- src/components/messagesTabData/MessagesTabData.jsx
+++ src/components/messagesTabData/MessagesTabData.jsx
@@ -1,36 +1,39 @@
-import { useState } from "react"
+import { useContext, useState } from "react"
 import styles from "./messagesTabData.module.css"
 import { useNavigate } from "react-router-dom"
+import { myContext } from "../../pages/HomePage"
 
 export default function MessagesTabData() {
     const navigate = useNavigate();
+    const { conversationsPreview } = useContext(myContext)
+    console.log(conversationsPreview)
 
     function handleNewMessageClick() {
         navigate('/new-message')
     }
 
-    const [messagePreview, setMessagePreview] = useState([
-        {
-            id: 1,
-            name: "God",
-            lastMessage: "I'm proud of you",
-            lastMessageTimeStamp: new Date().toLocaleTimeString('en-US', {
-                hour: 'numeric',
-                minute: '2-digit',
-                hour12: true,
-            })
-        },
-        {
-            id: 2,
-            name: "Imaginary",
-            lastMessage: "Hii",
-            lastMessageTimeStamp: new Date().toLocaleTimeString('en-US', {
-                hour: 'numeric',
-                minute: '2-digit',
-                hour12: true,
-            })
-        }
-    ])
+    // const [messagePreview, setMessagePreview] = useState([
+    //     {
+    //         id: 1,
+    //         name: "God",
+    //         lastMessage: "I'm proud of you",
+    //         lastMessageTimeStamp: new Date().toLocaleTimeString('en-US', {
+    //             hour: 'numeric',
+    //             minute: '2-digit',
+    //             hour12: true,
+    //         })
+    //     },
+    //     {
+    //         id: 2,
+    //         name: "Imaginary",
+    //         lastMessage: "Hii",
+    //         lastMessageTimeStamp: new Date().toLocaleTimeString('en-US', {
+    //             hour: 'numeric',
+    //             minute: '2-digit',
+    //             hour12: true,
+    //         })
+    //     }
+    // ])
 
     function handleMessagePreviewClick(recipientId) {
         navigate(`/conversation/${recipientId}`)
@@ -39,7 +42,7 @@ export default function MessagesTabData() {
     return (
         <div className={styles.contentWrapper}>
             <ul className={styles.listWrapper}>
-                {messagePreview.map(recipient => {
+                {conversationsPreview.map(recipient => {
                     return (
                         <li key={recipient.id} onClick={() => handleMessagePreviewClick(recipient.id)}>
                             <p>{recipient.name}</p>
